7deb97cf5af9180ef4f0a776b3787c2ff18b38f3
bling011 <ksambling@gmail.com>
2025-04-04 07:42:02 +0800

    Add deployment scripts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,7 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <Router basename="/FRONT-END-APP-DEV">
